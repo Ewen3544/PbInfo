7c93593e7a5976e447e38f8cdd285e0abded6c83
Ewen3544 <ewen.rascleansquer@orange.fr>
2025-03-01 15:39:53 +0100

    Update Noeud.cs

--- PbInfo1/Noeud.cs
+++ PbInfo1/Noeud.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PbInfo1
+namespace PbInfo
 {
-    internal class Noeud
+    /// <summary>
+    /// Représente un nœud dans un graphe.
+    /// </summary>
+    public class Noeud
     {
+        /// <summary>
+        /// Identifiant unique du nœud.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Constructeur de la classe Noeud.
+        /// </summary>
+        /// <param name="id">Identifiant du nœud.</param>
+        public Noeud(int id)
+        {
+            Id = id;
+        }
+        /// <summary>
+        /// Retourne une représentation du nœud.
+        /// </summary>
+        /// <returns>Chaîne représentant le nœud.</returns>
+        public override string ToString()
+        {
+            return $"Noeud: {Id}";
+        }
     }
 }
