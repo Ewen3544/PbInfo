9367283cafefd3d88f338b70f0137f845693fe03
Ewen3544 <ewen.rascleansquer@orange.fr>
2025-03-01 15:40:42 +0100

    Update Program.cs

--- PbInfo1/Program.cs
+++ PbInfo1/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace PbInfo1
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PbInfo
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string cheminFichier = @"C:\Users\ewenr\Downloads\karate.txt";
+            string[] lignes = File.ReadAllLines(cheminFichier);
+            string[] premiereLigne = lignes[0].Split(' ');
+            int nbSommets = int.Parse(premiereLigne[0]);
+            List<Lien> liens = new List<Lien>();
+            for (int i = 1; i < lignes.Length; i++)
+            {
+                string[] elementsLigne = lignes[i].Split(' ');
+                if (elementsLigne.Length == 2)
+                {
+                    int sommet1 = int.Parse(elementsLigne[0]);
+                    int sommet2 = int.Parse(elementsLigne[1]);
+                    liens.Add(new Lien(sommet1, sommet2));
+                }
+            }
+            Graphe graphe = new Graphe(liens, nbSommets);
+            graphe.AfficherListeAdjacence();
+            graphe.AfficherMatriceAdjacence();
+            graphe.Largeur(1);
+            graphe.Profondeur(1);
+            if (graphe.ContientUnCycle())
+                Console.WriteLine("Le graphe contient un cycle");
+            else
+                Console.WriteLine("Le graphe ne contient pas de cycle");
+            graphe.AnalyserGraphe();
+            graphe.VisualiserGraphe("graphe.png");
         }
     }
 }
