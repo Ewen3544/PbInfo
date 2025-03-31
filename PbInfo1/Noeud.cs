c48f3d6aecceaf376f3331f342b7393c563f4ae7
Sanjee979 <sanjeevan.sidhu@edu.devinci.fr>
2025-03-31 10:47:53 +0200

    Update Noeud.cs

--- PbInfo1/Noeud.cs
+++ PbInfo1/Noeud.cs
@@ -4,33 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace PbInfo
 {
     /// <summary>
-    /// Représente un nœud dans un graphe.
+    /// Représente un nœud générique dans un graphe.
     /// </summary>
-    public class Noeud
+    /// <typeparam name="T">Type des données stockées dans le nœud</typeparam>
+    public class Noeud<T>
     {
         /// <summary>
         /// Identifiant unique du nœud.
         /// </summary>
         public int Id { get; set; }
+        
+        /// <summary>
+        /// Données associées au nœud
+        /// </summary>
+        public T Data { get; set; }
+        
+        /// <summary>
+        /// Nom du nœud (pour affichage)
+        /// </summary>
+        public string Nom { get; set; }
+
+        /// <summary>
+        /// Coordonnée latitude
+        /// </summary>
+        public double Latitude { get; set; }
+        
+        /// <summary>
+        /// Coordonnée longitude
+        /// </summary>
+        public double Longitude { get; set; }
 
         /// <summary>
         /// Constructeur de la classe Noeud.
         /// </summary>
         /// <param name="id">Identifiant du nœud.</param>
-        public Noeud(int id)
+        /// <param name="data">Données du nœud</param>
+        /// <param name="nom">Nom du nœud</param>
+        /// <param name="latitude">Position latitude</param>
+        /// <param name="longitude">Position longitude</param>
+        public Noeud(int id, T data, string nom, double latitude, double longitude)
         {
             Id = id;
+            Data = data;
+            Nom = nom;
+            Latitude = latitude;
+            Longitude = longitude;
         }
+
         /// <summary>
         /// Retourne une représentation du nœud.
         /// </summary>
         /// <returns>Chaîne représentant le nœud.</returns>
         public override string ToString()
         {
-            return $"Noeud: {Id}";
+            return $"Noeud {Id}: {Nom} ({Latitude}, {Longitude})";
         }
     }
 }
