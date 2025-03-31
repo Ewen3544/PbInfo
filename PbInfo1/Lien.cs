0c9c61f2312e9cbeddd695aab6d309a4f2b87cda
Sanjee979 <sanjeevan.sidhu@edu.devinci.fr>
2025-03-31 12:20:10 +0200

    Update Lien.cs

--- PbInfo1/Lien.cs
+++ PbInfo1/Lien.cs
@@ -6,39 +6,19 @@ using System.Threading.Tasks;
 
 namespace PbInfo
 {
-    /// <summary>
-    /// Représente un lien entre deux nœuds dans un graphe.
-    /// </summary>
-    public class Lien
+    public class Arc
     {
-        /// <summary>
-        /// Identifiant du premier nœud du lien.
-        /// </summary>
-        public int Noeud1 { get; set; }
+        public int Destination { get; set; }
+        public double Poids { get; set; }
+        public string Ligne { get; set; }
+        public bool EstCorrespondance { get; set; }
 
-        /// <summary>
-        /// Identifiant du second nœud du lien.
-        /// </summary>
-        public int Noeud2 { get; set; }
-
-        /// <summary>
-        /// Constructeur de la classe Lien.
-        /// </summary>
-        /// <param name="noeud1">Premier nœud.</param>
-        /// <param name="noeud2">Second nœud.</param>
-        public Lien(int noeud1, int noeud2)
+        public Arc(int destination, double poids, string ligne, bool estCorrespondance = false)
         {
-            Noeud1 = noeud1;
-            Noeud2 = noeud2;
-        }
-
-        /// <summary>
-        /// Retourne une représentation du lien.
-        /// </summary>
-        /// <returns>Chaîne représentant le lien.</returns>
-        public override string ToString()
-        {
-            return $"{Noeud1} - {Noeud2}";
+            Destination = destination;
+            Poids = poids;
+            Ligne = ligne;
+            EstCorrespondance = estCorrespondance;
         }
     }
 }
